cf1234acfafd4af63c6928ddfc58e6bdeca6f405
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2021-01-05 19:35:12 +0100

    fix types

--- lib/route/bridge_vlan.c
+++ lib/route/bridge_vlan.c
@@ -1,12 +1,13 @@
 /* SPDX-License-Identifier: LGPL-2.1-only */
 /*
- * lib/route/mdb.c		Multicast Database
+ * lib/route/bridge_vlan.c		Bridge VLAN database
  */
 
 #include <netlink-private/netlink.h>
 #include <netlink/netlink.h>
 #include <linux/if_bridge.h>
 
+/** @cond SKIP */
 static struct nl_cache_ops rtnl_bridge_vlan_ops;
 static struct nl_object_ops bridge_vlan_obj_ops;
 /** @endcond */
@@ -16,10 +17,20 @@ static int bridge_vlan_request_update(struct nl_cache *cache, struct nl_sock *sk
 	return 0;
 }
 
+static struct nla_policy br_vlan_db_policy[BRIDGE_VLANDB_ENTRY_MAX + 1] = {
+	[BRIDGE_VLANDB_ENTRY_INFO]	= { .type = NLA_BINARY, 
+					    .minlen = sizeof(struct bridge_vlan_info),
+					    .maxlen = sizeof(struct bridge_vlan_info) },
+	[BRIDGE_VLANDB_ENTRY_RANGE]	= { .type = NLA_U16 },
+	[BRIDGE_VLANDB_ENTRY_STATE]	= { .type = NLA_U8 },
+	[BRIDGE_VLANDB_ENTRY_TUNNEL_INFO] = { .type = NLA_NESTED },
+};
+
 static int bridge_vlan_msg_parser(struct nl_cache_ops *ops, struct sockaddr_nl *who,
 			  struct nlmsghdr *nlh, struct nl_parser_param *pp)
 {
-	return 0;
+	int err = 0;
+	return err;
 }
 
 static struct nl_object_ops bridge_vlan_obj_ops = {
